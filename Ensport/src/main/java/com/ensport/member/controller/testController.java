89e59f41fa41c82ceab68053c1d4644f52ef5167
dhlove0417 <wpdntm1215@naver.com>
2023-10-31 16:51:30 +0900

    test commit

--- Ensport/src/main/java/com/ensport/member/controller/testController.java
+++ Ensport/src/main/java/com/ensport/member/controller/testController.java
@@ -27,6 +27,8 @@ public class testController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		//test
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
